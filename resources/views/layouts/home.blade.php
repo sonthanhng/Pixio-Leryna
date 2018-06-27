6528af58a85ba3203dd57a09396429b82cbc6641
unknown <sonnguyend10s@gmail.com>
2018-06-27 13:30:47 +0700

    update meta view tag desktop

--- resources/views/layouts/home.blade.php
+++ resources/views/layouts/home.blade.php
@@ -3,6 +3,8 @@
 	<html class="no-js">
 	<head>
 	<meta charset="utf-8">
+
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<title>Leryna</title>
 
