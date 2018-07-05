31d79fd87bfc90ef123d81ae769b496f28cb9017
unknown <sonnguyend10s@gmail.com>
2018-07-05 09:32:34 +0700

    update content

--- resources/views/UI/index.blade.php
+++ resources/views/UI/index.blade.php
@@ -25,7 +25,7 @@
 			<div class="row" id="about-us">
 				<div class="row" style="margin: 0px;">
 					<div class="col-md-8 col-xs-12 header-wrap">
-						<h2 class="to-animate header">Công ty xuất khẩu dép xốp hàng đầu Việt Nam</h2>
+						<h2 class="to-animate header">Nhà sản xuất tiên phong các sản phẩm tiệm nails salon</h2>
 					</div>
 				</div>
 				<div class="product-section row">
@@ -59,7 +59,7 @@
 				<div class="col-md-6 second-col col-xs-12">
 					<div class="about-leryna-wrap">
 						<p class="header">Về Leryna</p>
-						<p class="description">Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo. Nullam dictum felis eu pede mollis pretium. Integer tincidunt. Cras dapibus. Vivamus elementum semper nisi. Aenean vulputate eleifend te</p>
+						<p class="description">Tự hào là đơn vị hàng đầu trong lĩnh vực cung cấp sản phẩm dịch vụ Nails, với chất lượng và tiêu chuẩn Mỹ, Leryna mang sứ mệnh chăm sóc sắc đẹp tốt nhất cho phái nữ. Chúng tôi chiếm thị phần lớn trong thị trường phân phối các mặt hàng ngành Nails tại Mỹ & luôn cống hiến hết mình bằng khẩu hiệu “For your beauty & safety.”</p>
 					</div>
 				</div>
 			</div>
@@ -71,18 +71,18 @@
 			<div class="row">
 			<div class="col-md-4 col-xs-12 col">
 				<p class="number">01</p>
-				<p class="title">Chất lượng hàng đầu</p>
-				<p class="description">Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim.</p>
+				<p class="title">Chất lượng thật- giá trị thật</p>
+				<p class="description">Mỗi sản phẩm- dù là bé nhất- đều được gia công tỉ mỉ, kiểm duyệt gắt gao, quy trình rõ ràng. Tất cả tạo nên giá trị cho thương hiệu Leryna- khẳng định vị thế số 1 về chất lượng.</p>
 			</div>
 			<div class="col-md-4 col-xs-12 col">
 				<p class="number">02</p>
-				<p class="title">Uy tín số một</p>
-				<p class="description">Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim.</p>
+				<p class="title">Tiêu chuẩn cao- giá thành thấp</p>
+				<p class="description">Quy mô sản xuất lớn, đồng bộ hóa và công nghệ cao giúp Leryna có được lợi thế giá thành nổi bật. Không những thế, chúng tôi còn có các dịch vụ ưu đãi, chế độ hậu mãi chu đáo, hợp tác lần đầu - hợp tác mãi về sau.</p>
 			</div>
 			<div class="col-md-4 col-xs-12 col">
 				<p class="number">03</p>
-				<p class="title">Giá cả hợp lý</p>
-				<p class="description">Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim.</p>
+				<p class="title">Uy tín lâu năm</p>
+				<p class="description">Niềm tin của khách hàng chính là động lực và mục tiêu hướng tới của chúng tôi trên con đường kinh doanh và cống hiến cho sắc đẹp, sức khỏe phái nữ. Nhờ vậy, lời hứa thương hiệu của Leryna luôn trường tồn cùng thời gian.</p>
 			</div>
 		</div>
 		</div>
@@ -94,19 +94,19 @@
 				<!-- You may change the background color here. -->
 					<div class="item col">
 						<p class="number">01</p>
-						<p class="title">Chất lượng hàng đầu</p>
-						<p class="description">Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim.</p>
+						<p class="title">Chất lượng thật- giá trị thật</p>
+						<p class="description">Mỗi sản phẩm- dù là bé nhất- đều được gia công tỉ mỉ, kiểm duyệt gắt gao, quy trình rõ ràng. Tất cả tạo nên giá trị cho thương hiệu Leryna- khẳng định vị thế số 1 về chất lượng.</p>
 					</div>
 				<!-- You may change the background color here.  -->
 					<div class="item col">
 						<p class="number">02</p>
-						<p class="title">Uy tín số một</p>
-						<p class="description">Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim.</p>
+						<p class="title">Tiêu chuẩn cao- giá thành thấp</p>
+						<p class="description">Quy mô sản xuất lớn, đồng bộ hóa và công nghệ cao giúp Leryna có được lợi thế giá thành nổi bật. Không những thế, chúng tôi còn có các dịch vụ ưu đãi, chế độ hậu mãi chu đáo, hợp tác lần đầu - hợp tác mãi về sau.</p>
 					</div>
 					<div class="item col">
 						<p class="number">03</p>
-						<p class="title">Giá cả hợp lý</p>
-						<p class="description">Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim.</p>
+						<p class="title">Uy tín lâu năm</p>
+						<p class="description">Niềm tin của khách hàng chính là động lực và mục tiêu hướng tới của chúng tôi trên con đường kinh doanh và cống hiến cho sắc đẹp, sức khỏe phái nữ. Nhờ vậy, lời hứa thương hiệu của Leryna luôn trường tồn cùng thời gian.</p>
 					</div>
 			</div>
 		</div>
