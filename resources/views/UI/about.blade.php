ed0a8318b35bfd9d0d010d65bcf8607f4dc9003d
unknown <sonnguyend10s@gmail.com>
2018-07-05 12:23:01 +0700

    fix page about

--- resources/views/UI/about.blade.php
+++ resources/views/UI/about.blade.php
@@ -11,7 +11,7 @@
       <div class="col-md-6 second-col col-xs-12">
         <div class="about-leryna-wrap">
           <p class="header">Về Leryna</p>
-          <p class="description">Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo. Nullam dictum felis eu pede mollis pretium. Integer tincidunt. Cras dapibus. Vivamus elementum semper nisi. Aenean vulputate eleifend te</p>
+          <p class="description">Tự hào là đơn vị hàng đầu trong lĩnh vực cung cấp sản phẩm dịch vụ Nails, với chất lượng và tiêu chuẩn Mỹ, Leryna mang sứ mệnh chăm sóc sắc đẹp tốt nhất cho phái nữ. Chúng tôi chiếm thị phần lớn trong thị trường phân phối các mặt hàng ngành Nails tại Mỹ & luôn cống hiến hết mình bằng khẩu hiệu “For your beauty & safety.”</p>
         </div>
       </div>
     </div>
